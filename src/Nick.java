f384079a0374ebccba9a459faa03f7eb2d9dd89b
nporter1 <nporter1@ggc.edu>
2020-10-20 18:31:13 -0400

    This is Nick, committing/pushing first class

---
+++ src/Nick.java
@@ -0,0 +1,9 @@
+
+public class Nick
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is Nick's class");
+		//Mohfez & Ezekiel, create another class and commit/push it to the repository
+	}
+}
