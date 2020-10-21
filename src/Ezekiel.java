2ae25d6daa4b3775f22e817de0e58ad93ee2638a
Ezekiel Oluwatobi Akande <64216829+ezekieltoby@users.noreply.github.com>
2020-10-20 20:11:59 -0400

    Ezekiel Commiting

---
+++ src/Ezekiel.java
@@ -0,0 +1,5 @@
+public class Ezekiel {
+    public void printEzekiel(){
+        System.out.println("This is EZekiels class");
+    }
+}
--- src/Nick.java
+++ src/Nick.java
@@ -4,6 +4,8 @@ public class Nick
 	public static void main(String[] args)
 	{
 		System.out.println("This is Nick's class");
+		Ezekiel e = new Ezekiel();
+				e.printEzekiel();
 		//Mohfez & Ezekiel, create another class and commit/push it to the repository
 	}
 }
