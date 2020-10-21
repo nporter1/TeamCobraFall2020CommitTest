5e3c6e8c492345493d256586ca91c236e66e87e3
nporter1 <nporter1@ggc.edu>
2020-10-20 20:10:58 -0400

    test commit from Nick

--- src/Nick.java
+++ src/Nick.java
@@ -3,7 +3,7 @@ public class Nick
 {
 	public static void main(String[] args)
 	{
-		System.out.println("This is Nick's class");
+		System.out.println("This is Nick's class #test#");
 		//Mohfez & Ezekiel, create another class and commit/push it to the repository
 	}
 }
